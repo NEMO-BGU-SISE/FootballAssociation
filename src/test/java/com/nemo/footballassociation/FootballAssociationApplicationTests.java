0c4e2479ee33bd231128af9d5ac46ed3272b59ee
ofadlon <or5690@gmail.com>
2022-05-08 18:17:01 +0300

    commit

---
+++ src/test/java/com/nemo/footballassociation/FootballAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemo.footballassociation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballAssociationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
