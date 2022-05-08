db8c451285cf8ee63e0e4734a46d890d794046f1
ofadlon <or5690@gmail.com>
2022-05-08 18:06:53 +0300

    initial commit

---
+++ src/main/java/com/nemo/footballassociation/FootballAssociationApplication.java
@@ -0,0 +1,13 @@
+package com.nemo.footballassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballAssociationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballAssociationApplication.class, args);
+    }
+
+}
